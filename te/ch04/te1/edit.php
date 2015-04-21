7d158a75be99250fc2285b14588d309e5d193f20
Jonathan1472 <jonathan.rohrbach@cpnv.ch>
2015-04-21 20:10:45 +0200

    Dernière version

--- te/ch04/te1/edit.php
+++ te/ch04/te1/edit.php
@@ -1,46 +1,74 @@
+<!DOCTYPE html>
 <?php
-/*
-Te pratique Jonathan Rohrbach
+/*-------------------------------------------------------------
+* Te pratique Jonathan Rohrbach
+* Nom du fichier: edit.php
+* Auteur: Jonathan Rohrbach
+* Date : 13.03.2015
+* Description: Fichier permettant de modifier les informations de la tables cities.
+*--------------------------------------------------------------
 */
-// Constantes
+// Constantes pour se connecter à la base de donnée
 const DB_SERVER   = 'localhost';
 const DB_USER     = 'cpnv';
 const DB_PASSWORD = 'cpnv1234';
 const DB_NAME     = 'world';
 
-//const QUERY = "SELECT ID, Name, CountryCode, District FROM City WHERE CountryCode = 'CHE' ORDER BY Name";
-//
-const QUERY = "SELECT ID, Name, CountryCode, District FROM City WHERE CountryCode = 'CHE' ORDER BY Name";
-
-$dbh = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-
-
-if ($dbh->connect_errno) {
-
-    // Les erreurs de connexion sont dans connect_errno et connect_error
-    // A noter qu'en production, il serait judicieux, pour des raisons de
-    // sécurité, d'afficher les messages qui permettraient à un attaqueur de
-    // savoir quelle base de données nous utilisons.
-    $error_msg = sprintf('Problème de connexion : (%d) %s',
-                         $dbh->connect_errno, $dbh->connect_error);
-                         
-}
-
-if (!$result = $dbh->query(QUERY)) {
-    die("{$dbh->errno} : {$dbh->error}");
-    //printf("Select a retourné %d lignes.\n",$result->num_rows);    
-
-}
-
-
+//Déclaration des variables
+$id=$_GET['ID'];
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <title>Lecture d'enregistrements dans une table</title>
-  </head>
-  <body>
 
-  </body>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Modification</title>
+</head>
+  <body>
+      
+<?php
+
+    //Connexion à la base de donnée
+    $dbh = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+
+    //Gestion des erreurs de connexion à la base de donnée
+    if ($dbh->connect_errno) {
+        die("Problème de connexion ({$dbh->connect_errno}) " . $dbh->connect_error);
+    }
+
+//Formulaire permettant de modifier les informations
+
+    //Requête pour récupérer les informations
+    $query = "SELECT ID, Name, CountryCode, Population, District FROM City WHERE ID = $id ORDER BY Name";
+    
+    if ($result = $dbh->query($query)) {
+        
+        //Gestion des erreurs possibles lors de l'envoi de la requête
+        if (!$result) {
+            $message  = 'Requête incorrect : ' .mysqli_error() . "\n";
+            $message .= 'Requête complète : ' . $dbh;
+            die($message);
+        }
+        
+        //Affichage de la ville et de ses caractéristiques 
+        $city = $result->fetch_assoc();
+        ?>
+        
+<form method="post" action="update.php">
+    <p>ID:<input type="text" value="<?= $city['ID']; ?>" name="ID" /></p>
+    <p>Name:<input type="text" value="<?= $city['Name']; ?>" /></p>
+    <p>CountryCode:<input type="text" value="<?= $city['CountryCode']; ?>" /></p>
+    <p>District:<input type="text" value="<?= $city['District']; ?>" name="District" /></p>
+    <p>Population:<input type="text" value="<?= $city['Population']; ?>" name="Population" /></p>
+    <input type="submit" value="Modifier" />
+    <input type="submit" value="Enregistrer" />
+</form>
+
+<?php
+    }
+    $result->free();
+    
+    //Fermeture de la connexion établit avec la base de données
+    $dbh->close();
+    ?>
+</body>
 </html>
